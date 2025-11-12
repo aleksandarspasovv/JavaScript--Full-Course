23ce057dfd194ec8fae94071cb7f5e3e1cd78002
aleksandarspasovv <777alexander.spasov@gmail.com>
2025-11-13 00:29:23 +0200

    add: recursion

--- script3.js
+++ script3.js
@@ -19,3 +19,51 @@ buttons.forEach(button => {
         button.dataset.clicks = currentClicks + 1
     })
 })
+
+// Recursion 
+
+for (let i = 0; i < 10; i++){
+    console.log(i0)
+}
+
+//----------------------------------------
+function printNumber(number){
+    if (number > 10) return // i < 10
+    console.log(number)
+    printNumber(number + 1) // i++
+}
+
+printNumber(1) // let i = 0
+
+function sumNumber(number) {
+    if (number <= 0) return 0
+    number + sumNumber(number - 1)
+}
+
+sumNumber(2)
+
+//-----------------------
+
+const person = {
+    name: 'Alex',
+    friend: {
+        name: 'Nasko',
+        friend: {
+            name: 'Kalin'
+        }
+    }
+}
+
+let currentPerson = person
+
+while (currentPerson != null){ // goes all the way donw
+    console.log(currentPerson.friend)
+    currentPerson = currentPerson.friend
+}
+
+
+function currentPerson(person){
+    if (person == null) return
+    console.log(person.name)
+    currentPerson(person.friend)
+}
