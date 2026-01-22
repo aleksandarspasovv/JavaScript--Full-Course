3a2f558c5ca3881ed52fa3603372a3a30f19b322
aleksandarspasovv <777alexander.spasov@gmail.com>
2026-01-22 23:16:58 +0200

    add: optional chainig

---
+++ JavaScript Advanced/optionalChaining/script.js
@@ -0,0 +1,13 @@
+const alex = {
+    name: 'Alex',
+    age: 30,
+    address: {
+        street: 'Sofisko Shose 15',
+        city: 'Pernik',
+    },
+    hobbies: ['Football', 'Snowboarding']
+}
+
+console.log(person?.address?.city)
+console.log(person?.hobbies?.[0])
+
